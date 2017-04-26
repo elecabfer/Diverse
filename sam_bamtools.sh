52476ba90d82ca4ae4d125431560dd2453f2326a
elecabfer <elenam.cabello@gmail.com>
2017-04-26 10:52:51 +0200

    Create sam_bamtools.sh

---
+++ sam_bamtools.sh
@@ -0,0 +1,14 @@
+module add UHTS/Analysis/samtools/1.3;
+module add Emboss_package/EMBOSS/6.6.0;
+
+#filter mapped reads in bowtie2
+samtools view -b -F 4 bwt2_fast/"$i"_bowtie2_hg19_fast.sam	> bwt2_fast/"$i"_bowtie2_hg19_fast_mapped.bam
+
+#sam to bam
+samtools view -Sb bwt2_fast/"$i"_bowtie2_hg19_fast.sam >  bwt2_fast/"$i"_bowtie2_hg19_fast.bam
+
+#bam to fasta
+samtools fasta  bwt2_fast/"$i"_bowtie2_hg19_fast.bam  > bwt2_fast/"$i"_bowtie2_hg19_fast.fasta
+seqret -sequence bwt2_fast/"$i"_bowtie2_hg19_fast.bam -outseq bwt2_fast/"$i"_bowtie2_hg19_fast.fasta
+
+
